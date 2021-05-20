c6e31b528f9283a2f789096a808ba70fea282ee9
yeominji <lucky9834@naver.com>
2021-05-20 17:58:06 +0900

    Bookshop

--- jdbc_practices/src/main/java/bookshop/dao/BookDao.java
+++ jdbc_practices/src/main/java/bookshop/dao/BookDao.java
@@ -3,12 +3,14 @@ package bookshop.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import bookshop.vo.BookVo;
 
 public class BookDao {
-
 	public Boolean insert(BookVo vo) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -54,7 +56,94 @@ public class BookDao {
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
 		} 
-		
+
 		return conn;
+	}
+
+	public List<BookVo> findAll() {
+		List<BookVo> result = new ArrayList<>();
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+
+			String sql =
+				"   select a.no, a.title, a.status, b.name" + 
+			    "     from book a, author b" +
+			    "    where a.author_no = b.no" + 
+			    " order by a.no asc";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String title = rs.getString(2);
+				String status = rs.getString(3);
+				String authorName = rs.getString(4);
+
+				BookVo vo = new BookVo();
+				vo.setNo(no);
+				vo.setTitle(title);
+				vo.setStatus(status);
+				vo.setAuthorName(authorName);
+
+				result.add(vo);
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}		
+
+		return result;
+	}
+
+	public Boolean update(Long no, String status) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+
+		try {
+			conn = getConnection();
+
+			String sql = "update book set status = ? where no = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, status);
+			pstmt.setLong(2, no);
+
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
 	}	
 }
