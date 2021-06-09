1eb07bd2fb4db8ce855fa93860289e73d9031362
yeominji <yeominji@localhost>
2021-06-09 10:07:52 +0900

    book mall

--- jdbc_practices/src/main/java/bookshop/dao/BookDao.java
+++ jdbc_practices/src/main/java/bookshop/dao/BookDao.java
@@ -51,7 +51,7 @@ public class BookDao {
 		Connection conn = null;
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
-			String url = "jdbc:mysql://192.168.80.131:3307/webdb?charset=utf8";
+			String url = "jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
